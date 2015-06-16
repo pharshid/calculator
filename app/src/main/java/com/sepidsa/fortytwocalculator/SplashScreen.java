ad8c5e10e9fa7b6ac0b456336a5d9b5da2fbfca2
Farshid <farshid.imanipour@gmail.com>
2015-06-17 00:19:19 +0430

    splash method changed again

--- app/src/main/java/com/sepidsa/fortytwocalculator/SplashScreen.java
+++ app/src/main/java/com/sepidsa/fortytwocalculator/SplashScreen.java
@@ -3,32 +3,71 @@ package com.sepidsa.fortytwocalculator;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.MotionEvent;
 
 /**
  * Created by Farshid on 6/10/2015.
  */
 public class SplashScreen extends Activity {
 
+    /**
+     * The thread to process splash screen events
+     */
+    private Thread mSplashThread;
+
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Splash screen view
         setContentView(R.layout.splash);
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable()
-        {
-            @Override
-            public void run()
-            {
-                Intent intent = new Intent(SplashScreen.this,ParallaxPagerActivity.class);
-                startActivity(intent);
-                overridePendingTransition(R.anim.appear, R.anim.disappear);
-                finish();
-            }
-        }, 2000);
 
+        final SplashScreen sPlashScreen = this;
+
+        // The thread to wait for splash screen events
+        mSplashThread =  new Thread(){
+            @Override
+            public void run(){
+                try {
+                    synchronized(this){
+                        // Wait given period of time or exit on touch
+                        sleep(2000);
+                    }
+                }
+                catch(InterruptedException ex){
+                }
+
+                // Run next activity
+                Intent intent = new Intent(sPlashScreen, ParallaxPagerActivity.class);
+                startActivity(intent);
+
+            }
+        };
+
+        mSplashThread.start();
     }
 
+    /**
+     * Processes splash screen touch events
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent evt)
+    {
+        if(evt.getAction() == MotionEvent.ACTION_DOWN)
+        {
+            synchronized(mSplashThread){
+                mSplashThread.notifyAll();
+            }
+        }
+        return true;
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        overridePendingTransition(R.anim.appear, R.anim.disappear);
+        finish();
+
+    }
 }
